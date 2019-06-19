b935e89469a9e7fb7a620117e643f88e6ffb1c09
Pedro <phjfarias@hotmail.com>
2019-06-19 18:30:31 -0300

    Ajeitando oitava.java

--- jmusic/src/lume/oitava/Oitava.java
+++ jmusic/src/lume/oitava/Oitava.java
@@ -2,22 +2,17 @@ package lume.oitava;
 
 import jm.JMC;
 import jm.music.data.Note;
-import jm.util.Play;
 import java.util.ArrayList;
 
 public class Oitava implements JMC {
-	
-	@SuppressWarnings("rawtypes")
+
 	public static ArrayList AumentarOitava(ArrayList<Note> array) {
-		
-		
 		for(int i = 0; i < array.size(); i++) {
 			array.get(i).setPitch(array.get(i).getPitch() + 12);
 		}
 		return array;
 	}
-	
-	@SuppressWarnings("rawtypes")
+
 	public static ArrayList DiminuirOitava(ArrayList<Note> array) {
 		for(int k = 0; k < array.size(); k++) {
 			array.get(k).setPitch(array.get(k).getPitch() - 12);
