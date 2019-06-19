04c789acc92d1e569bfa9157ef9f2455c9657637
Pedro <phjfarias@hotmail.com>
2019-06-19 16:19:39 -0300

    Tratando error na instruments

--- jmusic/src/lume/instruments/Instruments.java
+++ jmusic/src/lume/instruments/Instruments.java
@@ -16,28 +16,44 @@ public class Instruments implements instrumento{
     }
 
     @Override
-    public void selectInst(int param) {
+    public void selectInst(int param){
         switch (param){
             case 0:
                 this.timbre = Timbres.Teclado;
+                System.out.println("O timbre mudou para: " + this.timbre);
                 break;
             case 1:
                 this.timbre = Timbres.Cordas;
+                System.out.println("O timbre mudou para: " + this.timbre);
                 break;
             case 2:
                 this.timbre = Timbres.Sopro;
+                System.out.println("O timbre mudou para: " + this.timbre);
                 break;
             case 3:
                 this.timbre = Timbres.Percursao;
+                System.out.println("O timbre mudou para: " + this.timbre);
+                break;
+            default:
+                System.out.println("Selecione um argumento válido");
                 break;
         }
-        System.out.println("O timbre mudou para: " + this.timbre);
+
+    }
+
+    // Para teste
+    public static void teste(){
+        Instruments inst = new Instruments();
+        System.out.println("O instrumento inicial é: " + inst.timbre);
+        inst.selectInst(1);
+        inst.selectInst(2);
+        inst.selectInst(3);
+        inst.selectInst(4);
     }
 
     public static void main(String[] args) {
-        Instruments inst = new Instruments();
-        System.out.println("O instrumento inicial é: " + inst.timbre);
-        inst.selectInst(2);
-
+        teste();
     }
-}   
+
+
+}
